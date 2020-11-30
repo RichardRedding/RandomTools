83946136c61b46a75962ec770617fbe651324ecf
Redding <rredding@sompo-intl.com>
2020-11-30 17:11:15 +0000

    accidental naming issue

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -3,13 +3,17 @@ function findAbscissa(targetlogmpdf :: Function, lower :: Float64, upper :: Floa
     flower, fupper = targetlogmpdf(lower), targetlogmpdf(upper)
     while !(isfinite(flower) && isfinite(fupper))
         d = (upper - lower) * shrinkRate
-        lower += d * 0.5
-        upper -= d * 0.5
-        flower, fupper = targetlogmpdf(lower), targetlogmpdf(upper)
+        if flower > fupper
+            upper -= d * 0.5
+            fupper = targetlogmpdf(upper)
+        else
+            lower += d * 0.5
+            flower = targetlogmpdf(lower)
+        end
     end
     
     middle = (lower + upper) * 0.5
-    while !((flower < targetlogmpdf(middle) > fupper) || (abs((middle - lower_) / (upper_ - lower_) - 0.5) > 0.45))
+    while !((flower < targetlogmpdf(middle) > fupper) || (abs((middle - lower) / (upper - lower) - 0.5) > 0.45))
         d = flower > fupper ? (lower - middle) : (upper - middle)
         middle += d * 0.5
     end
