b7b588a565bf66d5003ef3a67a895adcd584acaa
Redding <rredding@sompo-intl.com>
2020-12-16 10:01:32 +0000

    added unbounded proposal tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,21 +16,22 @@ testpdf(x :: Array{Tuple{Float64, Normal{Float64}}, 1}, y :: Number) = foldl((l,
 testlogpdf(x :: Array{Tuple{Float64, Normal{Float64}}, 1}, y :: Number) = log(testpdf(x, y))
 testcdf(x :: Array{Tuple{Float64, Normal{Float64}}, 1}, y :: Number) = foldl((l, (w, d)) -> l + w * Distributions.cdf(d, y), x, init = 0)
 
-convexSamplers = [
+### Test bounded domain
+boundedConvexSamplers = [
 (target, n) -> drawn(ARS(), ARSProposal(findAbscissa(target, -20.0, 20.0), target, -20.0, 20.0), target, 0.0, n),
 (target, n) -> drawn(ARMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -20.0, 20.0), target, 0.0, n),
 (target, n) -> drawn(IA2RMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -20.0, 20.0), target, 0.0, n),
 ]
 
-nonConvexSamplers = [
+boundedNonConvexSamplers = [
 (target, n) -> drawn(ARMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -20.0, 20.0), target, 0.0, n),
 (target, n) -> drawn(IA2RMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -20.0, 20.0), target, 0.0, n)
 ]
 
-convexTest = map(convexTargets) do target
-    map(convexSamplers) do sampler
+boundedConvexTest = map(convexTargets) do target
+    map(boundedConvexSamplers) do sampler
         ps = [0.005, 0.1, 0.25, 0.5, 0.75, 0.9, 0.995]
-        n = Int64(1e4)
+        n = Int64(1e5)
         x, prop = sampler(x -> testlogpdf(target, x), n)
         map(ps) do p
             estimate = p
@@ -40,10 +41,10 @@ convexTest = map(convexTargets) do target
     end
 end |> unfold
 
-nonConvexTest = map(nonConvexTargets) do target
-    map(nonConvexSamplers) do sampler
+boundedNonConvexTest = map(nonConvexTargets) do target
+    map(boundedNonConvexSamplers) do sampler
         ps = [0.005, 0.1, 0.25, 0.5, 0.75, 0.9, 0.995]
-        n = Int64(1e4)
+        n = Int64(1e5)
         x, prop = sampler(x -> testlogpdf(target, x), n)
         map(ps) do p
             estimate = p
@@ -53,5 +54,46 @@ nonConvexTest = map(nonConvexTargets) do target
     end
 end |> unfold
 
-@test sum(convexTest) == 0
-@test sum(nonConvexTest) == 0
+@test sum(boundedConvexTest) == 0
+@test sum(boundedNonConvexTest) == 0
+
+### Test unbounded domain
+unboundedConvexSamplers = [
+(target, n) -> drawn(ARS(), ARSProposal(findAbscissa(target, -20.0, 20.0), target, -Inf, Inf), target, 0.0, n),
+(target, n) -> drawn(ARMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -Inf, Inf), target, 0.0, n),
+(target, n) -> drawn(IA2RMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -Inf, Inf), target, 0.0, n),
+]
+
+unboundedNonConvexSamplers = [
+(target, n) -> drawn(ARMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -Inf, Inf), target, 0.0, n),
+(target, n) -> drawn(IA2RMS(), ARMSProposal(findAbscissa(target, -20.0, 20.0), target, -Inf, Inf), target, 0.0, n)
+]
+
+unboundedConvexTest = map(convexTargets) do target
+    map(unboundedConvexSamplers) do sampler
+        ps = [0.005, 0.1, 0.25, 0.5, 0.75, 0.9, 0.995]
+        n = Int64(1e5)
+        x, prop = sampler(x -> testlogpdf(target, x), n)
+        map(ps) do p
+            estimate = p
+            exact = testcdf(target, Distributions.quantile(x, p))
+            (abs(estimate - exact) / sqrt(exact * (1 - exact) / n)) > 2
+        end
+    end
+end |> unfold
+
+unboundedNonConvexTest = map(nonConvexTargets) do target
+    map(unboundedNonConvexSamplers) do sampler
+        ps = [0.005, 0.1, 0.25, 0.5, 0.75, 0.9, 0.995]
+        n = Int64(1e5)
+        x, prop = sampler(x -> testlogpdf(target, x), n)
+        map(ps) do p
+            estimate = p
+            exact = testcdf(target, Distributions.quantile(x, p))
+            (abs(estimate - exact) / sqrt(exact * (1 - exact) / n)) > 2
+        end
+    end
+end |> unfold
+
+@test sum(unboundedConvexTest) == 0
+@test sum(unboundedNonConvexTest) == 0
